3ede39fdce3eb20e2832e44736a363176fff2bef
almtav08 <alexmartinez3107@gmail.com>
2023-03-10 11:38:11 +0100

    added asmacag game

--- games/asmacag/asmacag_game_state.py
+++ games/asmacag/asmacag_game_state.py
@@ -81,7 +81,7 @@ class AsmacagGameState(GameState):
     def get_current_turn(self) -> int:
         return self.current_turn
     
-    def get_game_parameters(self) -> 'GameParameters':
+    def get_game_parameters(self) -> 'AsmacagGameParameters':
         return self.game_parameters
     
     def get_player_0_score(self) -> int:
