8ddc96e903a55ff906304a6b6f677f89aefe792a
almtav08 <alexmartinez3107@gmail.com>
2023-03-22 15:15:46 +0100

    added script for parallel execution

---
+++ play_all.py
@@ -0,0 +1,11 @@
+from collections import defaultdict
+from joblib import Parallel, delayed
+from pathlib import Path
+import os
+
+def play_game(game_conf: str):
+    os.system("python play_n_games.py " + game_conf)
+
+if __name__ == "__main__":
+    conf_files = *map(str, Path("conf").rglob("*.json")),
+    Parallel(n_jobs=6)(delayed(play_game)(i) for i in conf_files)
