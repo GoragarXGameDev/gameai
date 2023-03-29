a9e0ce98b1fd7d02bfcd2e35c6910ed0714bd3a6
almtav08 <alexmartinez3107@gmail.com>
2023-03-29 15:06:07 +0200

    added bridge burning and non exploring mcts

--- play_game.py
+++ play_game.py
@@ -1,3 +1,4 @@
+import func_timeout
 from games.asmacag import *
 from games.hero_academy import *
 from games.tank_war import *
@@ -8,6 +9,8 @@ if __name__ == '__main__':
     # Players parameters
     ## Greedy Action
     greedy_hueristic = SimpleHeuristic()
+    ## Greedy Turn
+    greedyt_hueristic = SimpleHeuristic()    
     ## Monte Carlo Tree Search
     mcts_heuristic = SimpleHeuristic()
     ## Online Evolution
@@ -17,22 +20,23 @@ if __name__ == '__main__':
     ntboe_heuristic = SimpleHeuristic()
 
     # Common parameters
-    budget = 5                                  # time to think for the players (in seconds)
+    budget = 1                                  # time to think for the players (in seconds)
+    rounds = 100                                # number of rounds to play
     verbose = True                              # whether to print messages
     enforce_time = True                         # whether the player time to think is going to be enforced
     save_name = "out/sample_output.txt"         # where the game is going to be saved, can be None
 
     # ASMACAG parameters
-    parameters = AsmacagGameParameters() 
-    forward_model = AsmacagForwardModel()
-    fitness_asmacag = AsmacagFitnessEvaluator(ntboe_heuristic)
-    game = AsmacagGame(parameters, forward_model)
+    # parameters = AsmacagGameParameters() 
+    # forward_model = AsmacagForwardModel()
+    # fitness_asmacag = AsmacagFitnessEvaluator(ntboe_heuristic)
+    # game = AsmacagGame(parameters, forward_model)
 
     # Hero Academy parameters
-    #parameters = HeroAcademyGameParameters() 
-    #forward_model = HeroAcademyForwardModel()
-    #fitness_heroac = HeroAcademyFitnessEvaluator(ntboe_heuristic)
-    #game = HeroAcademyGame(parameters, forward_model)
+    parameters = HeroAcademyGameParameters() 
+    forward_model = HeroAcademyForwardModel()
+    fitness_heroac = HeroAcademyFitnessEvaluator(ntboe_heuristic)
+    game = HeroAcademyGame(parameters, forward_model)
 
     # Tank war parameters
     # parameters = TankWarGameParameters() 
@@ -43,11 +47,14 @@ if __name__ == '__main__':
     # Common players
     random = RandomPlayer()
     greedy = GreedyActionPlayer(greedy_hueristic)
+    greedyt = GreedyTurnPlayer(greedyt_hueristic)
     mcts = MontecarloTreeSearchPlayer(mcts_heuristic, 8)
+    bbmcts = BridgeBurningMontecarloTreeSearchPlayer(mcts_heuristic, 8)
+    nemcts = NonExploringMontecarloTreeSearchPlayer(mcts_heuristic)
     oe = OnlineEvolutionPlayer(oe_heuristic, 125, 0.15, 0.15)
 
     # ASMACAG players
-    ntboe_asmacag = NTupleBanditOnlineEvolutionPlayer(ntboe_heuristic, fitness_asmacag, dimensions, 8, 5, 0.55, 1000)
+    # ntboe_asmacag = NTupleBanditOnlineEvolutionPlayer(ntboe_heuristic, fitness_asmacag, dimensions, 8, 5, 0.55, 1000)
 
     # Hero Academy players
     # ntboe_heroac = NTupleBanditOnlineEvolutionPlayer(ntboe_heuristic, fitness_heroac, dimensions, 8, 5, 0.55, 1000)
@@ -55,11 +62,11 @@ if __name__ == '__main__':
     # Tank war players
     # ntboe_tankwar = NTupleBanditOnlineEvolutionPlayer(ntboe_heuristic, fitness_tankwar, dimensions, 8, 5, 0.55, 1000)
     
-    players = [oe, mcts]                       # list of players
+    players = [mcts, nemcts]                       # list of players
 
     game.set_save_file(save_name)
 
-    game.run(players[0], players[1], budget, verbose, enforce_time)
+    game.run(players[0], players[1], budget, rounds, verbose, enforce_time)
 
     if verbose:
         print("")
