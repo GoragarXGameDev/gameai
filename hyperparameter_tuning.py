338d23f98cf92071b52375a347d77e82f090aef7
Raul Montoliu <montoliu@uji.es>
2023-04-08 11:16:16 +0200

    hyper para mcts

--- hyperparameter_tuning.py
+++ hyperparameter_tuning.py
@@ -1,9 +1,52 @@
+import math
 from sys import argv
+
+from joblib import Parallel, delayed
 
 from games.asmacag import AsmacagGameParameters, AsmacagForwardModel, AsmacagGame
 from heuristics import SimpleHeuristic
+from players import MontecarloTreeSearchPlayer
 from utils import GameEvaluatorOE, Ntbea
-import smtplib
+
+
+from utils.GameEvaluator import GameEvaluator
+
+
+def evaluate(param_c: float, evaluator: GameEvaluator, n_games: int, budget: float, rounds:int):
+    mcts_player = MontecarloTreeSearchPlayer(SimpleHeuristic(), param_c)
+    score = evaluator.evaluate(mcts_player, n_games, budget, rounds)
+    return param_c, score
+
+
+def do_asmacag_mcts(budget:float):
+    # ASMACAG parameters
+    parameters = AsmacagGameParameters()
+    forward_model = AsmacagForwardModel()
+    game = AsmacagGame(parameters, forward_model)
+
+    asmacag_evaluator = GameEvaluator(game, SimpleHeuristic())
+    params_c = [round(0.35 + i*0.35, 2) for i in range(30)]
+    n_games = 100
+    rounds = 100
+    cores = 8
+
+    results = Parallel(n_jobs=cores)(
+        delayed(evaluate)(c, asmacag_evaluator, n_games, budget, rounds)
+        for c in params_c
+    )
+
+    best_c = None
+    best_score = -math.inf
+    out_str = ""
+    for param_c, score in results:
+        if score >= best_score:
+            best_score = score
+            best_c = param_c
+        out_str += str(param_c) + "," + str(score) + " \n"
+
+    out_str += "ASMACAG,MCTS," + str(budget) + "," + str(best_c)
+    with open("out/hyper_asmacag_mcts_" + str(budget) + ".txt", "w") as f:
+        f.write(out_str + " \n")
 
 
 def do_asmacag_oe(budget: float):
@@ -53,6 +96,8 @@ if __name__ == '__main__':
     if game_name == "asmacag":
         if algorithm == "oe":
             do_asmacag_oe(budget)
+        elif algorithm == "mcts":
+            do_asmacag_mcts(budget)
 
 
 
